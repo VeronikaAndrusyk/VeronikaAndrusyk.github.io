9195854e79166b342f2009428cc122fe48db659e
VeronikaAndrusyk <127151583+VeronikaAndrusyk@users.noreply.github.com>
2023-09-19 20:51:53 +0300

    Add files via upload

---
+++ C#lab1/lab1(1).cs
@@ -0,0 +1,31 @@
+﻿/*Дано структуру даних (колекцію) відповідно до варіанта. Додати
+зазначену кількість елементів, які описують відповідну предметну
+область. Вивести всі елементи на консоль в прямому та зворотному
+порядку. Вивести кількість елементів у колекції. Очистити колекцію.*/
+using System;
+class Program
+{
+    static void Main()
+    {
+        string[] cities = { "Львiв", "Київ", "Харкiв", "Херсон", "Ужгород", "Суми", "Броди", "Миколаїв", "Рiвне", "Житомир" };
+        Console.WriteLine("Мiста України");
+        foreach (string city in cities)
+        {
+            Console.WriteLine(city);
+        }
+
+        Array.Reverse(cities);
+        Console.WriteLine("\n Мiста України у зворотньому порядку");
+        foreach (string city in cities)
+        {
+            Console.WriteLine(city);
+        }
+
+        Console.WriteLine("\nКiлькiсть елементiв у масивi: " + cities.Length);
+        Array.Clear(cities, 0, cities.Length);
+    }
+    
+
+    
+
+}
---
+++ C#lab1/lab1(2).cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введiть кiлькiсть елементiв для стеку: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Stack<int> stack = new Stack<int>();
+
+        Console.WriteLine(" Введiть елементи для стеку:");
+        for (int i = 0; i < n; i++)
+        {
+            int element = int.Parse(Console.ReadLine());
+            stack.Push(element);
+        }
+
+        /*Random random = new Random();
+        for (int i = 0; i < n; i++)
+        {
+            int randomNumber = random.Next(1, 101); 
+            stack.Push(randomNumber);
+        }*/
+
+        Console.WriteLine("Елементи стеку:");
+        foreach (int item in stack)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+
+        int sum = 0;
+        int count = 0;
+        foreach (int item in stack)
+        {
+            if (item % 5 == 0)
+            {
+                sum += item;
+                count++;
+            }
+        }
+
+        if (count > 0)
+        {
+            Console.WriteLine($"Сума чисел, що дiляться без остачi на 5: {sum}");
+            Console.WriteLine($"Кiлькiсть таких чисел: {count}");
+        }
+        else
+        {
+            Console.WriteLine("Чисел, що дiляться без остачi на 5, в стеку немає.");
+        }
+    }
+
+
+
+}
