bbd6c8a0d1430adc58a45bcb2890a0b77b0d72f7
VeronikaAndrusyk <127151583+VeronikaAndrusyk@users.noreply.github.com>
2023-10-12 15:56:57 +0300

    Update Program.cs

--- С#LAB2/С#LAB2/Program.cs
+++ С#LAB2/С#LAB2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -28,23 +28,62 @@ public class Program
             new Student(18, new DateTime(2009, 6, 18), "Garcia", "Group B"),
             new Student(19, new DateTime(1991, 7, 19), "Martinez", "Group A"),
             new Student(20, new DateTime(2010, 8, 20), "Robinson", "Group B")
-            
+
         };
 
         //  рейтинг для студентів
         students[0].Ratings.Add(new Student.Rating(students[0].StudentCode, "Вища математика", 4, 3));
+        students[0].Ratings.Add(new Student.Rating(students[0].StudentCode, "Дискретка",      5, 3));
+        students[0].Ratings.Add(new Student.Rating(students[0].StudentCode, "Прогрмування",    2, 3));
+
         students[1].Ratings.Add(new Student.Rating(students[1].StudentCode, "Вища математика", 2, 5));
+        students[1].Ratings.Add(new Student.Rating(students[1].StudentCode, "Дискретка",       3, 5));
+        students[1].Ratings.Add(new Student.Rating(students[1].StudentCode, "Прогрмування", 2, 5));
+
         students[2].Ratings.Add(new Student.Rating(students[2].StudentCode, "Вища математика", 3, 3));
+        students[2].Ratings.Add(new Student.Rating(students[2].StudentCode, "Дискретка",       1, 3));
+        students[2].Ratings.Add(new Student.Rating(students[2].StudentCode, "Прогрмування", 3, 3));
+
         students[3].Ratings.Add(new Student.Rating(students[3].StudentCode, "Вища математика", 2, 5));
+        students[3].Ratings.Add(new Student.Rating(students[3].StudentCode, "Дискретка",       2, 5));
+        students[3].Ratings.Add(new Student.Rating(students[3].StudentCode, "Прогрмування", 2, 5));
+
         students[4].Ratings.Add(new Student.Rating(students[4].StudentCode, "Вища математика", 4, 3));
+        students[4].Ratings.Add(new Student.Rating(students[4].StudentCode, "Дискретка",       4, 3));
+        students[4].Ratings.Add(new Student.Rating(students[4].StudentCode, "Прогрмування", 4, 3));
+
         students[5].Ratings.Add(new Student.Rating(students[5].StudentCode, "Вища математика", 2, 5));
+        students[5].Ratings.Add(new Student.Rating(students[5].StudentCode, "Дискретка",       2, 5));
+        students[5].Ratings.Add(new Student.Rating(students[5].StudentCode, "Прогрмування", 2, 5));
+
         students[6].Ratings.Add(new Student.Rating(students[6].StudentCode, "Вища математика", 5, 3));
+        students[6].Ratings.Add(new Student.Rating(students[6].StudentCode, "Дискретка",       5, 3));
+        students[6].Ratings.Add(new Student.Rating(students[6].StudentCode, "Прогрмування", 5, 3));
+
         students[7].Ratings.Add(new Student.Rating(students[7].StudentCode, "Вища математика", 2, 5));
+        students[7].Ratings.Add(new Student.Rating(students[7].StudentCode, "Дискретка",       2, 5));
+        students[7].Ratings.Add(new Student.Rating(students[7].StudentCode, "Прогрмування", 2, 5));
+
         students[8].Ratings.Add(new Student.Rating(students[8].StudentCode, "Вища математика", 4, 3));
+        students[8].Ratings.Add(new Student.Rating(students[8].StudentCode, "Дискретка",       4, 3));
+        students[8].Ratings.Add(new Student.Rating(students[8].StudentCode, "Прогрмування", 4, 3));
+
         students[9].Ratings.Add(new Student.Rating(students[9].StudentCode, "Вища математика", 2, 5));
+        students[9].Ratings.Add(new Student.Rating(students[9].StudentCode, "Дискретка",       2, 5));
+        students[9].Ratings.Add(new Student.Rating(students[9].StudentCode, "Прогрмування", 2, 5));
+
         students[10].Ratings.Add(new Student.Rating(students[10].StudentCode, "Вища математика", 1, 3));
+        students[10].Ratings.Add(new Student.Rating(students[10].StudentCode, "Дискретка",       1, 3));
+        students[10].Ratings.Add(new Student.Rating(students[10].StudentCode, "Прогрмування", 1, 3));
+
         students[11].Ratings.Add(new Student.Rating(students[11].StudentCode, "Вища математика", 2, 5));
+        students[11].Ratings.Add(new Student.Rating(students[11].StudentCode, "Дискретка",       2, 5));
+        students[11].Ratings.Add(new Student.Rating(students[11].StudentCode, "Прогрмування", 2, 5));
+
         students[12].Ratings.Add(new Student.Rating(students[12].StudentCode, "Вища математика", 4, 3));
+        students[12].Ratings.Add(new Student.Rating(students[12].StudentCode, "Дискретка",       4, 3));
+        students[12].Ratings.Add(new Student.Rating(students[12].StudentCode, "Прогрмування", 4, 3));
+
         students[13].Ratings.Add(new Student.Rating(students[13].StudentCode, "Вища математика", 2, 5));
         students[14].Ratings.Add(new Student.Rating(students[14].StudentCode, "Вища математика", 5, 3));
         students[15].Ratings.Add(new Student.Rating(students[15].StudentCode, "Вища математика", 2, 5));
@@ -53,7 +92,7 @@ public class Program
         students[18].Ratings.Add(new Student.Rating(students[18].StudentCode, "Вища математика", 4, 3));
         students[19].Ratings.Add(new Student.Rating(students[19].StudentCode, "Вища математика", 2, 5));
 
-       
+
         TaskA(students);
         TaskB(students);
         TaskC(students);
@@ -75,12 +114,15 @@ public class Program
     public static void TaskB(List<Student> students)
     {
         var topStudents = students
-            .Where(student => student.Ratings.Any(rating => rating.Subject == "Вища математика"))//відфільтровує студентів
-            .OrderByDescending(student =>student.Ratings.Where(rating => rating.Subject == "Вища математика")
-            .Average(rating => rating.Module1Score))
+            .Where(student => student.Ratings.Any(rating =>
+                rating.Subject == "Вища математика" || rating.Subject == "Дискретка" || rating.Subject == "Програмування"))
+            .OrderByDescending(student =>
+                student.Ratings.Where(rating =>
+                    rating.Subject == "Вища математика" || rating.Subject == "Дискретка" || rating.Subject == "Програмування")
+                    .Average(rating => rating.Module1Score))
             .Select(student => student.LastName);
 
-        Console.WriteLine("Студенти з найвищим середнiм рейтингом за перший модуль:");
+        Console.WriteLine("Студенти з найвищим середнім рейтингом за перший модуль у предметах Вища математика, Programming, або Diskretka:");
         foreach (var lastName in topStudents)
         {
             Console.WriteLine($"Прiзвище: {lastName}");
@@ -90,16 +132,16 @@ public class Program
     public static void TaskC(List<Student> students)
     {
         var groupSubjectAverage = students
-            .SelectMany(student => student.Ratings, (student, rating) => new { student, rating })//об'єднує оцінки всіх студентів з їх групами та предметами
-            .GroupBy(x => new { x.student.Group, x.rating.Subject })//групує елементи з попередньої послідовності
-            .Select(group => new//проектує результати групування в новий анонімний тип даних
+            .SelectMany(student => student.Ratings, (student, rating) => new { student, rating })
+            .GroupBy(x => new { x.student.Group, x.rating.Subject })
+            .Select(group => new
             {
                 Group = group.Key.Group,
                 Subject = group.Key.Subject,
                 AverageRating = group.Average(x => x.rating.Module1Score)
             });
 
-        Console.WriteLine("Список з середнiми рейтингами для груп з предмету:");
+        Console.WriteLine("Список з середнiми рейтингами для груп з усіх предметів:");
         foreach (var item in groupSubjectAverage)
         {
             Console.WriteLine($"Група: {item.Group}, Предмет: {item.Subject}, Середнiй рейтинг: {item.AverageRating:F2}");
@@ -107,4 +149,3 @@ public class Program
     }
 
 }
-
