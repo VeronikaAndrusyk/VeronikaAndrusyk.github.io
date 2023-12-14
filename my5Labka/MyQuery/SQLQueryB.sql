48c9ff3fd9983f3540d5c851b6da96b2082e807b
VeronikaAndrusyk <127151583+VeronikaAndrusyk@users.noreply.github.com>
2023-12-14 03:13:08 +0200

    Add files via upload

---
+++ my5Labka/MyQuery/SQLQueryB.sql
@@ -0,0 +1,16 @@
+﻿/*b) запит на вибірку з використанням спеціальних функцій: LIKE, IS NULL, IN, BETWEEN;*/
+
+
+/* студенти з факультету 'Фізика' та прізвище починається на 'Петр'*/
+  SELECT * FROM Students WHERE  [Group] = 'A123' AND [City] LIKE 'Ки%';
+
+/* студенти, у яких місце роботи не вказане */
+  SELECT * FROM Students WHERE [Workplace] IS NULL;
+
+
+/* студенти з групи 'A123' і 'B456'*/
+  SELECT * FROM Students WHERE [Group] IN ('A123', 'B456');
+
+
+/* Вибрати студентів, які народилися від 1998 до 2000 року*/
+  SELECT * FROM Students WHERE [BirthYear] BETWEEN 1998 AND 2000;
---
+++ my5Labka/MyQuery/SQLQueryE.sql
@@ -0,0 +1,5 @@
+﻿/*e) Запит з використанням обчислювального поля: */
+
+/*  середній бал студентів разом із різницею від максимального балу*/
+
+SELECT [AverageGrade], [AverageGrade] - (SELECT MAX([AverageGrade]) FROM Students) AS 'Різниця від максимального балу' FROM Students;
